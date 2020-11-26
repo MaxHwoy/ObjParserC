49edf3744575b217dbf7eaca03fdd8d659ff6384
MaxHwoy <max.hwoy@gmail.com>
2020-11-26 02:52:12 -0800

    Initial commit.

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "ObjParserC"
+	location ".\\build\\"
+	startproject "ObjParserC"
+
+	targetdir "%{wks.location}\\bin\\%{cfg.buildcfg}\\"
+	objdir "%{wks.location}\\obj\\%{cfg.buildcfg}\\%{prj.name}\\"
+	buildlog "%{wks.location}\\obj\\%{cfg.buildcfg}\\%{prj.name}.log"
+
+	largeaddressaware "on"
+	editandcontinue "off"
+	staticruntime "on"
+
+	systemversion "latest"
+	characterset "mbcs"
+	warnings "extra"
+
+	syslibdirs {
+		-- Any static libs we link against go here.
+	}
+
+	includedirs {
+		".\\src\\",
+		".\\deps\\LinearC\\",
+	}
+
+	buildoptions {
+		"/Zm200",
+		"/utf-8",
+		"/std:c++latest",
+	}
+
+	flags {
+		"no64bitchecks",
+		"shadowedvariables",
+		"noincrementallink",
+		"undefinedidentifiers",
+		"multiprocessorcompile",
+	}
+
+	platforms {
+		"x86",
+		"x64",
+	}
+
+	configurations {
+		"debug",
+		"release",
+	}
+
+	configuration "debug"
+		defines "DEBUG"
+		optimize "debug"
+		runtime "debug"
+		symbols "on"
+
+	configuration "release"
+		defines "NDEBUG"
+		optimize "full"
+		runtime "release"
+		symbols "on"
+
+	project "ObjParserC"
+		targetname "ObjParserC"
+		dependson "LinearC"
+
+		language "c++"
+		kind "staticlib"
+
+		pchheader "stdafx.hpp"
+		pchsource "src/stdafx.cpp"
+
+		defines {
+			"NOMINMAX",
+			"_CRT_SECURE_NO_WARNINGS",
+		}
+
+		files {
+			".\\src\\**.hpp",
+			".\\src\\**.cpp",
+		}
+
+		links {
+			"LinearC",
+		}
+
+		includedirs {
+			".\\src\\",
+		}
+
+	group "Dependencies"
+
+	project "LinearC"
+		targetname "LinearC"
+		warnings "off"
+
+		language "c++"
+		kind "staticlib"
+
+		files {
+			".\\deps\\LinearC\\src\\**.h",
+			".\\deps\\LinearC\\src\\**.cpp",
+		}
+
+		includedirs {
+			".\\deps\\LinearC\\src\\",
+		}
